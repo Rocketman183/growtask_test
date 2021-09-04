8a51662845bb22bcb58cfbea80433c5c50a16b52
Dimoffmart <dimoffmart@gmail.com>
2021-09-05 01:34:41 +0500

    Сделал раздел с модальным окном

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,5 +1,10 @@
 $('.toggle').on('click', function () {
 	$('.header__menu-items').slideToggle();
+
+	$('toggle').fadeIn(3000, function () {
+		$('.toggle').removeClass('fa-times');
+		$('#menu-icon').addClass('fa-bars').fadeOut(3000);
+	});
 })
 
 console.log('yep');
