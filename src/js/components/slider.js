4ad7d8a04e5a76bfee0677c9b8ee5280e9a3a129
Dimoffmart <dimoffmart@gmail.com>
2021-09-05 23:38:34 +0500

    Сделал слайдер

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,4 +4,11 @@ const swiper = new Swiper('.swiper', {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev"
 	},
+	breakpoints: {
+		768: {
+			slidesPerView: 1.5,
+			spaceBetween: 150,
+			centeredSlides: true,
+		}
+	}
 });
